fed58c8075a5ccb4ae3cd9e1f2f97d578ba80f0f
Luis <mrluiscortes@gmail.com>
2018-07-04 21:10:17 -0600

    removing old files

--- mylogging.py
+++
@@ -1,5 +0,0 @@
-import logbook as LB
-import sys 
-
-LB.FileHandler("invaders.log").push_application()
-log = LB.Logger("invaders")
--- rotateit.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-im = Image.open("stardust.png")
-im.thumbnail( (96,96) )
-for idx, angle in enumerate(range(0,180,9*2)):
-    print(idx,angle)
-    t = im.rotate(angle)
-    t.save("images/explosion/stardust_sm_%02d.png"%idx,"png")
-
